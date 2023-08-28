1f333cdef30577ff9a82dedef3e00671b07478b1
Methrat0n <merlingoulet@spyrals.fr>
2023-08-28 10:29:50 +0200

    fix(electron): asar unpacked path rewrite

--- packages/embedded-postgres/src/binary.ts
+++ packages/embedded-postgres/src/binary.ts
@@ -47,4 +47,26 @@ function getBinaries(): Binaries {
     }
 }
 
-export default getBinaries;
+function environmentModification(): Binaries {
+  const rawBinaries = getBinaries();
+
+  // For electron apps running this in the main process
+  const isRunningInElectron = process.versions.hasOwnProperty('electron')
+  if(isRunningInElectron) {
+    const isAsarPackaged = isRunningInElectron && process.mainModule.filename.indexOf('app.asar') === -1;
+
+    if(!isAsarPackaged) {
+      return rawBinaries
+    }
+
+    // As stated in the asar documentation, executables should be unpacked
+    // https://www.electronjs.org/docs/latest/tutorial/asar-archives#executing-binaries-inside-asar-archive
+    return {
+      pg_ctl: rawBinaries.pg_ctl.replace('app.asar', 'app.asar.unpacked'),
+      postgres: rawBinaries.postgres.replace('app.asar', 'app.asar.unpacked'),
+      initdb: rawBinaries.initdb.replace('app.asar', 'app.asar.unpacked')
+    }
+  }
+}
+
+export default environmentModification;
